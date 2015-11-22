9946f330377b5d1166afe5c1f844a4282a5d4fc2
Robert <robkegge@hotmail.com>
2015-11-22 21:54:33 +0100

    how do i php

--- email.php
+++ email.php
@@ -1,43 +1,43 @@
 <?php
- / Data variables /
+  ini_set('display_errors', 1);
+
+ // Data variables 
  $sendName  = $_POST['name'];
  $sendAdres  = $_POST['adres'];
  $sendMail  = $_POST['email'];
  $sendZIP  = $_POST['zip'];
  $sendWP   = $_POST['wp'];
  $sendTF  = $_POST['tele'];
- $sendMSG   = $_POST['comment'];
+ $sendMSG   = $_POST['messa'];
  $sendHuur1 = $_POST['vdate'];
  $sendHuur2 = $_POST['tdate'];
- $sendTent = $_POST['tent'];
+ $sendTent = $_POST['dropdown-selection'];
  $sendExtra = $_POST['extra'];
  $sendinva = $_POST['inva'];
  $sendbagw = $_POST['bagw'];
 
+ if(isset($_POST['extra']))
+{
+  if (is_array($_POST['extra']))
+  {
+    foreach ($_POST['extra'] as $value) {
+       echo $value;
+     }
+  }
+   else
+  {
+    $value = $_POST['extra'];
+    echo $value;
+  }
+}
 
-echo $sendName;
-echo $sendAdres;
-echo $sendMail;
-echo $sendZIP;
-echo $sendWP;
-echo $sendTF;
-echo $sendMSG;
-echo $sendHuur1;
-echo $sendHuur2;
-echo $sendTent;
-echo $sendExtra;
-echo $sendinva;
-echo $sendbagw;
 
-?>
-/*
- / Configuration /
  $onderwerp = 'Offerte aanvraag';
 
  $verzendNaar = 'robkegge@hotmail.com';
 
  $body = "
-  <h1>Persoonlijke gegevens</h1><br><br>
+  <h3>Persoonlijke gegevens</h3><br><br>
   Naam: $sendName<br>
   Adres: $sendAdres<br>
   Postcode: $sendZIP<br>
@@ -45,15 +45,22 @@ echo $sendbagw;
   Telefoonnummer: $sendTF<br>
   E-mail: $sendMail<br><br>
 
-  <h1>Huurperiode</h1><br><br>
+  <h3>Huurperiode</h3><br><br>
   Van: $sendHuur1<br>
   Tot: $sendHuur2<br><br>
 
-  <h1>Tent</h1><br><br>
+  <h3>Tent</h3><br><br>
   Tent: $sendTent<br><br>
 
-  <h1>Extra's</h1><br><br>
-  Tent: $sendExtra<br><br>
+  <h3>Extra's</h3><br><br>
+  Gekozen Extra's:
+  
+  Inventaris voor $sendinva personen.<br>
+
+  Bagagewagen: $sendbagw <br> <br>
+
+  Extra vragen / opmerkingen: $sendMSG <br>
+
  ";
 
  $headers = "From: $sendName <$sendMail> \r\n";
@@ -70,6 +77,31 @@ echo $sendbagw;
     echo "Er is iets fout gegaan";
  }
 
+
 ?>
 
-*/
+<!--
+echo $sendName;
+echo "<br>";
+echo $sendAdres;
+echo "<br>";
+echo $sendMail;
+echo "<br>";
+echo $sendZIP;
+echo "<br>";
+echo $sendWP;
+echo "<br>";
+echo $sendTF;
+echo "<br>";
+echo htmlspecialchars($sendMSG);
+echo "<br>";
+echo $sendHuur1;
+echo "<br>";
+echo $sendHuur2;
+echo "<br>";
+echo $sendTent;
+echo "<br>";
+echo $sendinva;
+echo "<br>";
+echo $sendbagw;
+-->
